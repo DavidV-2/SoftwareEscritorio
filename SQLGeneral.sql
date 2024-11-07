7a9821bbea73374378133ae491d2211306c30585
David E. Velez Gonzalez <152334918+DavidV-2@users.noreply.github.com>
2024-11-07 14:03:59 -0500

    Update SQLGeneral.sql

--- SQLGeneral.sql
+++ SQLGeneral.sql
@@ -1,4 +1,4 @@
-﻿﻿
+﻿
 create Database Bd_Foto_Correa
 
 
@@ -24,12 +24,13 @@ FechaRegistro datetime default getdate()
 
 go
 
-create table PROVEEDOR(
-IdProveedor int primary key identity,
+create table USUARIO(
+IdUsuario int primary key identity,
 Documento varchar(50),
-RazonSocial varchar(50),
+NombreCompleto varchar(50),
 Correo varchar(50),
-Telefono varchar(50),
+Clave varchar(50),
+IdRol int references ROL(IdRol),
 Estado bit,
 FechaRegistro datetime default getdate()
 )
@@ -48,19 +49,6 @@ FechaRegistro datetime default getdate()
 
 go
 
-create table USUARIO(
-IdUsuario int primary key identity,
-Documento varchar(50),
-NombreCompleto varchar(50),
-Correo varchar(50),
-Clave varchar(50),
-IdRol int references ROL(IdRol),
-Estado bit,
-FechaRegistro datetime default getdate()
-)
-
-go
-
 create table CATEGORIA(
 IdCategoria int primary key identity,
 Descripcion varchar(100),
@@ -73,80 +61,91 @@ go
 create table PRODUCTO(
 IdProducto int primary key identity,
 Codigo varchar(50),
-Nombre varchar(50),
 Descripcion varchar(50),
 IdCategoria int references CATEGORIA(IdCategoria),
-Stock int not null default 0,
-PrecioCompra decimal(10,2) default 0,
 PrecioVenta decimal(10,2) default 0,
 Estado bit,
 FechaRegistro datetime default getdate()
 )
-
 go
 
-create table COMPRA(
-IdCompra int primary key identity,
-IdUsuario int references USUARIO(IdUsuario),
-IdProveedor int references PROVEEDOR(IdProveedor),
-TipoDocumento varchar(50),
-NumeroDocumento varchar(50),
-MontoTotal decimal(10,2),
-FechaRegistro datetime default getdate()
-)
-
-
-go
-
-
-create table DETALLE_COMPRA(
-IdDetalleCompra int primary key identity,
-IdCompra int references COMPRA(IdCompra),
-IdProducto int references PRODUCTO(IdProducto),
-PrecioCompra decimal(10,2) default 0,
+create table PRODUCTO_DETAL(
+IdProducto int primary key identity,
+Descripcion varchar(50),
 PrecioVenta decimal(10,2) default 0,
-Cantidad int,
-MontoTotal decimal(10,2),
-FechaRegistro datetime default getdate()
+Estado bit
 )
 
 go
 
 create table VENTA(
 IdVenta int primary key identity,
-IdUsuario int references USUARIO(IdUsuario),
-TipoDocumento varchar(50),
-NumeroDocumento varchar(50),
-DocumentoCliente varchar(50),
-NombreCliente varchar(100),
-MontoPago decimal(10,2),
-MontoCambio decimal(10,2),
-MontoTotal decimal(10,2),
+IdProducto int references PRODUCTO(IdProducto),
+DatosCliente varchar(50),
+Telefono varchar(50),
+Especificaciones varchar(50),
+ValorVenta decimal(10,2),
+ValorPagado decimal(10,2),
+ValorResta decimal(10,2),
+SubTotal decimal(10,2),
+Cantidad int,
+EstadoDeuda varchar (50),
+NumeroConsecutivo int,
 FechaRegistro datetime default getdate()
 )
 
-
 go
 
+create table Venta_Detal(
+IdVentaDetal int primary key identity,
+IdProducto int references PRODUCTO_DETAL(IdProducto),
+NumeroConsecutivo int,
+Producto varchar(100),
+Cantidad int,
+SubTotal decimal(10,2),
+PrecioVenta decimal(10,2) default 0,
+FechaRegistro datetime default getdate()
+)
+
+go
 
 create table DETALLE_VENTA(
 IdDetalleVenta int primary key identity,
 IdVenta int references VENTA(IdVenta),
 IdProducto int references PRODUCTO(IdProducto),
-PrecioVenta decimal(10,2),
+Telefono varchar(50),
+Especificaciones varchar(50),
 Cantidad int,
+ValorVenta decimal(10,2),
+ValorPagado decimal(10,2),
+ValorResta decimal(10,2),
 SubTotal decimal(10,2),
 FechaRegistro datetime default getdate()
 )
 
 go
 
-create table NEGOCIO(
-IdNegocio int primary key,
-Nombre varchar(60),
-RUC varchar(60),
+create table Detalle_VentaDetal(
+IdDetVentaDetal int primary key identity,
+Cantidad int,
+SubTotal decimal(10,2),
+IdProducto int references PRODUCTO_DETAL(IdProducto),
+PrecioVenta decimal(10,2),
+FechaRegistro datetime default getdate()
+)
+
+go
+
+create table Evento(
+IdEvento int primary key,
+DatosCliente varchar(60),
+Telefono varchar(60),
 Direccion varchar(60),
-Logo varbinary(max) NULL
+DescripcionEvento varchar (100),
+ValorVenta decimal(10,2),
+ValorPagado decimal(10,2),
+ValorResta decimal(10),
+Estado bit
 )
 
 go
@@ -347,10 +346,10 @@ GO
 /* ---------- PROCEDIMIENTOS PARA PRODUCTO -----------------*/
 
 create PROC sp_RegistrarProducto(
-@Codigo varchar(20),
-@Nombre varchar(30),
-@Descripcion varchar(30),
 @IdCategoria int,
+@Codigo varchar(20),
+@Descripcion varchar(30),
+@PrecioVenta decimal(10,2),
 @Estado bit,
 @Resultado int output,
 @Mensaje varchar(500) output
@@ -359,7 +358,8 @@ begin
 	SET @Resultado = 0
 	IF NOT EXISTS (SELECT * FROM producto WHERE Codigo = @Codigo)
 	begin
-		insert into producto(Codigo,Nombre,Descripcion,IdCategoria,Estado) values (@Codigo,@Nombre,@Descripcion,@IdCategoria,@Estado)
+		insert into producto(IdCategoria,Codigo,Descripcion,PrecioVenta,Estado) 
+		values (@IdCategoria,@Codigo,@Descripcion,@PrecioVenta,@Estado)
 		set @Resultado = SCOPE_IDENTITY()
 	end
 	ELSE
@@ -372,7 +372,6 @@ GO
 create procedure sp_ModificarProducto(
 @IdProducto int,
 @Codigo varchar(20),
-@Nombre varchar(30),
 @Descripcion varchar(30),
 @IdCategoria int,
 @Estado bit,
@@ -382,11 +381,10 @@ create procedure sp_ModificarProducto(
 as
 begin
 	SET @Resultado = 1
-	IF NOT EXISTS (SELECT * FROM PRODUCTO WHERE codigo = @Codigo and IdProducto != @IdProducto)
+	IF NOT EXISTS (SELECT * FROM PRODUCTO WHERE Codigo = @Codigo and IdProducto != @IdProducto)
 		
 		update PRODUCTO set
-		codigo = @Codigo,
-		Nombre = @Nombre,
+		Codigo = @Codigo,
 		Descripcion = @Descripcion,
 		IdCategoria = @IdCategoria,
 		Estado = @Estado
@@ -411,16 +409,6 @@ begin
 	set @Respuesta = 0
 	set @Mensaje = ''
 	declare @pasoreglas bit = 1
-
-	IF EXISTS (SELECT * FROM DETALLE_COMPRA dc 
-	INNER JOIN PRODUCTO p ON p.IdProducto = dc.IdProducto
-	WHERE p.IdProducto = @IdProducto
-	)
-	BEGIN
-		set @pasoreglas = 0
-		set @Respuesta = 0
-		set @Mensaje = @Mensaje + 'No se puede eliminar porque se encuentra relacionado a una COMPRA\n' 
-	END
 
 	IF EXISTS (SELECT * FROM DETALLE_VENTA dv
 	INNER JOIN PRODUCTO p ON p.IdProducto = dv.IdProducto
@@ -498,267 +486,6 @@ begin
 	end
 end
 
-GO
-
-/* ---------- PROCEDIMIENTOS PARA PROVEEDOR -----------------*/
-
-create PROC sp_RegistrarProveedor(
-@Documento varchar(50),
-@RazonSocial varchar(50),
-@Correo varchar(50),
-@Telefono varchar(50),
-@Estado bit,
-@Resultado int output,
-@Mensaje varchar(500) output
-)as
-begin
-	SET @Resultado = 0
-	DECLARE @IDPERSONA INT 
-	IF NOT EXISTS (SELECT * FROM PROVEEDOR WHERE Documento = @Documento)
-	begin
-		insert into PROVEEDOR(Documento,RazonSocial,Correo,Telefono,Estado) values (
-		@Documento,@RazonSocial,@Correo,@Telefono,@Estado)
-
-		set @Resultado = SCOPE_IDENTITY()
-	end
-	else
-		set @Mensaje = 'El numero de documento ya existe'
-end
-
-GO
-
-create PROC sp_ModificarProveedor(
-@IdProveedor int,
-@Documento varchar(50),
-@RazonSocial varchar(50),
-@Correo varchar(50),
-@Telefono varchar(50),
-@Estado bit,
-@Resultado bit output,
-@Mensaje varchar(500) output
-)as
-begin
-	SET @Resultado = 1
-	DECLARE @IDPERSONA INT 
-	IF NOT EXISTS (SELECT * FROM PROVEEDOR WHERE Documento = @Documento and IdProveedor != @IdProveedor)
-	begin
-		update PROVEEDOR set
-		Documento = @Documento,
-		RazonSocial = @RazonSocial,
-		Correo = @Correo,
-		Telefono = @Telefono,
-		Estado = @Estado
-		where IdProveedor = @IdProveedor
-	end
-	else
-	begin
-		SET @Resultado = 0
-		set @Mensaje = 'El numero de documento ya existe'
-	end
-end
-
-
-go
-
-create procedure sp_EliminarProveedor(
-@IdProveedor int,
-@Resultado bit output,
-@Mensaje varchar(500) output
-)
-as
-begin
-	SET @Resultado = 1
-	IF NOT EXISTS (
-	 select *  from PROVEEDOR p
-	 inner join COMPRA c on p.IdProveedor = c.IdProveedor
-	 where p.IdProveedor = @IdProveedor
-	)
-	begin
-	 delete top(1) from PROVEEDOR where IdProveedor = @IdProveedor
-	end
-	ELSE
-	begin
-		SET @Resultado = 0
-		set @Mensaje = 'El proveedor se encuentara relacionado a una compra'
-	end
-
-end
-
-go
-
-/* PROCESOS PARA REGISTRAR UNA COMPRA */
-
-CREATE TYPE [dbo].[EDetalle_Compra] AS TABLE(
-	[IdProducto] int NULL,
-	[PrecioCompra] decimal(18,2) NULL,
-	[PrecioVenta] decimal(18,2) NULL,
-	[Cantidad] int NULL,
-	[MontoTotal] decimal(18,2) NULL
-)
-
-
-GO
-
-
-CREATE PROCEDURE sp_RegistrarCompra(
-@IdUsuario int,
-@IdProveedor int,
-@TipoDocumento varchar(500),
-@NumeroDocumento varchar(500),
-@MontoTotal decimal(18,2),
-@DetalleCompra [EDetalle_Compra] READONLY,
-@Resultado bit output,
-@Mensaje varchar(500) output
-)
-as
-begin
-	
-	begin try
-
-		declare @idcompra int = 0
-		set @Resultado = 1
-		set @Mensaje = ''
-
-		begin transaction registro
-
-		insert into COMPRA(IdUsuario,IdProveedor,TipoDocumento,NumeroDocumento,MontoTotal)
-		values(@IdUsuario,@IdProveedor,@TipoDocumento,@NumeroDocumento,@MontoTotal)
-
-		set @idcompra = SCOPE_IDENTITY()
-
-		insert into DETALLE_COMPRA(IdCompra,IdProducto,PrecioCompra,PrecioVenta,Cantidad,MontoTotal)
-		select @idcompra,IdProducto,PrecioCompra,PrecioVenta,Cantidad,MontoTotal from @DetalleCompra
-
-
-		update p set p.Stock = p.Stock + dc.Cantidad, 
-		p.PrecioCompra = dc.PrecioCompra,
-		p.PrecioVenta = dc.PrecioVenta
-		from PRODUCTO p
-		inner join @DetalleCompra dc on dc.IdProducto= p.IdProducto
-
-		commit transaction registro
-
-
-	end try
-	begin catch
-		set @Resultado = 0
-		set @Mensaje = ERROR_MESSAGE()
-		rollback transaction registro
-	end catch
-
-end
-
-
-GO
-
-/* PROCESOS PARA REGISTRAR UNA VENTA */
-
-CREATE TYPE [dbo].[EDetalle_Venta] AS TABLE(
-	[IdProducto] int NULL,
-	[PrecioVenta] decimal(18,2) NULL,
-	[Cantidad] int NULL,
-	[SubTotal] decimal(18,2) NULL
-)
-
-
-GO
-
-
-create procedure usp_RegistrarVenta(
-@IdUsuario int,
-@TipoDocumento varchar(500),
-@NumeroDocumento varchar(500),
-@DocumentoCliente varchar(500),
-@NombreCliente varchar(500),
-@MontoPago decimal(18,2),
-@MontoCambio decimal(18,2),
-@MontoTotal decimal(18,2),
-@DetalleVenta [EDetalle_Venta] READONLY,                                      
-@Resultado bit output,
-@Mensaje varchar(500) output
-)
-as
-begin
-	
-	begin try
-
-		declare @idventa int = 0
-		set @Resultado = 1
-		set @Mensaje = ''
-
-		begin  transaction registro
-
-		insert into VENTA(IdUsuario,TipoDocumento,NumeroDocumento,DocumentoCliente,NombreCliente,MontoPago,MontoCambio,MontoTotal)
-		values(@IdUsuario,@TipoDocumento,@NumeroDocumento,@DocumentoCliente,@NombreCliente,@MontoPago,@MontoCambio,@MontoTotal)
-
-		set @idventa = SCOPE_IDENTITY()
-
-		insert into DETALLE_VENTA(IdVenta,IdProducto,PrecioVenta,Cantidad,SubTotal)
-		select @idventa,IdProducto,PrecioVenta,Cantidad,SubTotal from @DetalleVenta
-
-		commit transaction registro
-
-	end try
-	begin catch
-		set @Resultado = 0
-		set @Mensaje = ERROR_MESSAGE()
-		rollback transaction registro
-	end catch
-
-end
-
-go
-
-
-create PROC sp_ReporteCompras(
- @fechainicio varchar(10),
- @fechafin varchar(10),
- @idproveedor int
- )
-  as
- begin
-
-  SET DATEFORMAT dmy;
-   select 
- convert(char(10),c.FechaRegistro,103)[FechaRegistro],c.TipoDocumento,c.NumeroDocumento,c.MontoTotal,
- u.NombreCompleto[UsuarioRegistro],
- pr.Documento[DocumentoProveedor],pr.RazonSocial,
- p.Codigo[CodigoProducto],p.Nombre[NombreProducto],ca.Descripcion[Categoria],dc.PrecioCompra,dc.PrecioVenta,dc.Cantidad,dc.MontoTotal[SubTotal]
- from COMPRA c
- inner join USUARIO u on u.IdUsuario = c.IdUsuario
- inner join PROVEEDOR pr on pr.IdProveedor = c.IdProveedor
- inner join DETALLE_COMPRA dc on dc.IdCompra = c.IdCompra
- inner join PRODUCTO p on p.IdProducto = dc.IdProducto
- inner join CATEGORIA ca on ca.IdCategoria = p.IdCategoria
- where CONVERT(date,c.FechaRegistro) between @fechainicio and @fechafin
- and pr.IdProveedor = iif(@idproveedor=0,pr.IdProveedor,@idproveedor)
- end
-
- go
-
- CREATE PROC sp_ReporteVentas(
- @fechainicio varchar(10),
- @fechafin varchar(10)
- )
- as
- begin
- SET DATEFORMAT dmy;  
- select 
- convert(char(10),v.FechaRegistro,103)[FechaRegistro],v.TipoDocumento,v.NumeroDocumento,v.MontoTotal,
- u.NombreCompleto[UsuarioRegistro],
- v.DocumentoCliente,v.NombreCliente,
- p.Codigo[CodigoProducto],p.Nombre[NombreProducto],ca.Descripcion[Categoria],dv.PrecioVenta,dv.Cantidad,dv.SubTotal
- from VENTA v
- inner join USUARIO u on u.IdUsuario = v.IdUsuario
- inner join DETALLE_VENTA dv on dv.IdVenta = v.IdVenta
- inner join PRODUCTO p on p.IdProducto = dv.IdProducto
- inner join CATEGORIA ca on ca.IdCategoria = p.IdCategoria
- where CONVERT(date,v.FechaRegistro) between @fechainicio and @fechafin
-end
-
-
-
-
 /****************** INSERTAMOS REGISTROS A LAS TABLAS ******************/
 /*---------------------------------------------------------------------*/
 
@@ -788,25 +515,24 @@ GO
  GO
 
   insert into PERMISO(IdRol,NombreMenu) values
-  (1,'menuusuarios'),
-  (1,'menumantenedor'),
-  (1,'menuventas'),
-  (1,'menucompras'),
-  (1,'menuclientes'),
-  (1,'menuproveedores'),
-  (1,'menureportes'),
-  (1,'menuacercade')
+  (1,'menu_ventas'),
+  (1,'menu_detalle_ventas'),
+  (1,'menu_agenda'),
+  (1,'menu_productos'),
+  (1,'menu_clientes'),
+  (1,'menu_usuarios'),
+  (1,'Menu_Acercade')
 
   GO
 
   insert into PERMISO(IdRol,NombreMenu) values
-  (2,'menuventas'),
-  (2,'menucompras'),
-  (2,'menuclientes'),
-  (2,'menuproveedores'),
-  (2,'menuacercade')
+  (2,'menu_ventas'),
+  (2,'menu_detalle_ventas'),
+  (2,'menu_agenda'),
+  (2,'menu_productos'),
+  (2,'Menu_Acercade')
 
   GO
 
-  insert into NEGOCIO(IdNegocio,Nombre,RUC,Direccion,Logo) values
-  (1,'Codigo Estudiante','20202020','av. codigo estudiante 123',null)
+  insert into Evento(IdEvento,DatosCliente,Telefono,Direccion,DescripcionEvento,ValorVenta,ValorPagado,ValorResta) values
+  ('Sujeto Prueba','300000','av35. codigo estudiante 123','prueba','250000','250000','0',1)
